1063956a660cfdc0189b22c19b44b1d9a14d4cfc
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-02-22 08:56:06 +0100

    solved day 6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,13 +1,24 @@
 (ns advent-of-code.day-06
   "AOC 2020 Day 6"
-  (:require [advent-of-code.util :refer [split-lines]]))
+  (:require [advent-of-code.util :refer [split-lines split-p]]
+            [clojure.string :as string]
+            [clojure.set :refer [intersection]]))
 
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (reduce + (map (fn [group]
+                   (-> group
+                       (string/replace #"\n" "")
+                       (set)
+                       (count)))
+                 (split-p input))))
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (reduce + (map (fn [group]
+                   (count (apply intersection 
+                                 (for [l (split-lines group)]
+                                   (set l)))))
+                 (split-p input))))
