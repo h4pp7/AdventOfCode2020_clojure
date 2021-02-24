6998b50cd22b1e07d36243c4b02becbdd05f5045
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-02-24 14:00:24 +0100

    added input and tests for day 7

--- test/advent_of_code/day_07_test.clj
+++ test/advent_of_code/day_07_test.clj
@@ -4,9 +4,21 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 4]
     (is (= expected (part-1 (slurp (resource "day-07-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 32]
     (is (= expected (part-2 (slurp (resource "day-07-example.txt")))))))
+
+(deftest part2-2
+  (let [expected 126]
+    (is (= expected (part-2 (slurp (resource "day-07-example-2.txt")))))))
+
+(deftest part1-solution
+  (let [expected 289]
+    (is (= expected (part-1 (slurp (resource "day-07.txt")))))))
+
+(deftest part2-solution
+  (let [expected 30055]
+    (is (= expected (part-2 (slurp (resource "day-07.txt")))))))
