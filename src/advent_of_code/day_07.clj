9fa4e5ded8896cb4f73bbaa747b534e4d9a5387a
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-02-25 02:59:26 +0100

    adjacency graph functionality for day 7

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -31,3 +31,33 @@
     source-key
     (assoc (get graph source-key) 
            target-key value)))
+
+(defn trimmed-keyword [k] (keyword (clojure.string/replace k #"\s" "-")))
+  
+(defn parse-line-to-map
+  [line graph]
+  (let [line (parse-line line)
+        outer-bag (trimmed-keyword (line 0))
+        graph (add-node graph outer-bag)
+        inner-bags (line 1)
+        mapper (fn 
+                 [graph bag]
+                 (add-edge graph 
+                           outer-bag 
+                           (trimmed-keyword (bag 0)) 
+                           (bag 1)))]
+    (loop [inner-bags inner-bags
+           graph graph]
+      (if (empty? inner-bags)
+        graph
+        (recur (rest inner-bags)
+               (mapper graph (first inner-bags)))))))
+
+(defn build-graph
+  [lines]
+  (loop [lines lines
+         graph nil]
+    (if (empty? lines)
+      graph
+      (recur (rest lines)
+             (parse-line-to-map (first lines) graph)))))
