be7c591db8fa284fb927079599cf91e2775f11bf
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-03-02 16:35:21 +0100

    mapping of bags in both directions

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -24,34 +24,40 @@
 
 (defn trimmed-keyword [k] (keyword (clojure.string/replace k #"\s" "-")))
   
+(defn map-bags
+  "makes map from bags to bags they contain"
+  [graph outer-bag inner-bag direction]
+  (cond (= "out-in" direction) (add-edge graph 
+                                         outer-bag
+                                         (trimmed-keyword (inner-bag 0)) 
+                                         (inner-bag 1))
+        (= "in-out" direction) (add-edge graph 
+                                         (trimmed-keyword (inner-bag 0)) 
+                                         outer-bag
+                                         (inner-bag 1))))
+
 (defn parse-line-to-map
-  [line graph]
+  [line graph direction]
   (let [line       (parse-line line)
         outer-bag  (trimmed-keyword (line 0))
         graph      (add-node graph outer-bag)
-        inner-bags (line 1)
-        mapper     (fn 
-                     [graph bag]
-                     (add-edge graph 
-                               (trimmed-keyword (bag 0)) 
-                               outer-bag 
-                               (bag 1)))]
+        inner-bags (line 1)]
     (loop [inner-bags inner-bags
            graph      graph]
       (if (empty? inner-bags)
         graph
         (recur (rest inner-bags)
-               (mapper graph (first inner-bags)))))))
+               (map-bags graph outer-bag (first inner-bags) direction))))))
 
 (defn build-adj-list
   "Constructs an adjacency list. Bags mapped to their containers."
-  [lines]
+  [lines direction]
   (loop [lines lines
          graph nil]
     (if (empty? lines)
       graph
       (recur (rest lines)
-             (parse-line-to-map (first lines) graph)))))
+             (parse-line-to-map (first lines) graph direction)))))
 
 (defn visited? [node visited] (some #(= % node) visited))
 
@@ -76,7 +82,7 @@
   [input]
   (->> input
        (split-lines)
-       (build-adj-list)
+       (build-adj-list "in-out")
        (graph-dfs :shiny-gold)
        (count)
        (dec)))
