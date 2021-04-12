29fcafc6a4d18d64a356ef0ddfa7e017f7e5db80
Yann Wilhelm <yannwilhelm@yahoo.de>
2021-04-12 19:33:12 +0200

    draft for day 8

--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,6 +1,19 @@
 (ns advent-of-code.day-08
   "AOC 2020 Day 8"
-  (:require [advent-of-code.util :refer [split-lines]]))
+  (:require [advent-of-code.util :refer [split-lines]]
+            [clojure.string :refer [split]]))
+
+(defn parse-instruction
+  [instruction]
+  (let [i (split instruction #" ")]
+    [(symbol (first i)) (read-string (last i))]))  
+
+(defn acc [state value]
+  {:ACC (+ (:ACC state) value) :PC (inc (:PC state))})
+      
+(defn step
+  [state instr]
+  ()) 
 
 (defn part-1
   "Day 08 Part 1"
--- test/advent_of_code/day_08_test.clj
+++ test/advent_of_code/day_08_test.clj
@@ -1,7 +1,30 @@
 (ns advent-of-code.day-08-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.day-08 :refer [part-1 part-2]]
+            [advent-of-code.day-08 :refer [part-1 part-2 parse-instruction step acc]]
             [clojure.java.io :refer [resource]]))
+
+(deftest parse-pos-instruction
+  (let [expected ['acc 1]]
+    (is (= expected (parse-instruction "acc +1")))))
+
+(deftest parse-neg-instruction
+  (let [expected ['jmp -4]]
+    (is (= expected (parse-instruction "jmp -4")))))
+
+(deftest parse-zero-instruction
+  (let [expected ['nop +0]]
+    (is (= expected (parse-instruction "nop +0")))))
+
+(deftest test-acc
+  (let [expected  {:ACC 4 :PC 1}
+        state     {:ACC 0 :PC 0}]
+    (is (= expected (acc state 4)))))
+
+(deftest steptest
+  (let [expected  {:ACC 4 :PC 1}
+        state     {:ACC 0 :PC 0}
+        instr     "acc +4"]
+    (is (= expected (step state instr)))))
 
 (deftest part1
   (let [expected nil]
